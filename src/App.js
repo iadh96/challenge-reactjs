a4748f2f55787e50eb3e16f02dc193c821dab87b
iadh96 <iadhboussetta@gmail.com>
2021-11-09 14:35:14 +0100

    challenge

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './css/style.css';
+import Projj from './Components/Projects';
+import Navone from './Components/Navbar';
+import Ab from './Components/About';
+import Conta from './Components/Contact';
+import Foo from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+<Navone />
+<Projj />
+<Ab />
+<Conta />
+<Foo />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Projects.js
@@ -0,0 +1,65 @@
+import image1 from "../images/proj1.jpg"
+import image2 from "../images/proj2.jpg"
+import image3 from "../images/proj3.jpg"
+import image4 from "../images/proj4.jpg"
+function Projj() {
+      return(
+
+        <section id="projects">
+        <h2 class="text-important">Projects</h2>
+        <div class="projects-container">
+          <div class="project-card">
+            <img src={image1} alt="project" />
+            <h3>Project 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+              aliquam.
+            </p>
+            <p><a href="#">Github Link</a></p>
+          </div>
+          
+          <div class="project-card">
+            <img src={image2} alt="project" />
+            <h3>Project 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+              aliquam.
+            </p>
+            <p><a href="#">Github Link</a></p>
+          </div>
+          <div class="project-card">
+            <img src={image3} alt="project" />
+            <h3>Project 3</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+              aliquam.
+            </p>
+            <p><a href="#">Github Link</a></p>
+          </div>
+          <div class="project-card">
+            <img src={image4} alt="project" />
+            <h3>Project 4</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+              aliquam.
+            </p>
+            <p><a href="#">Github Link</a></p>
+          </div>
+        </div>
+      </section>
+     
+      )
+}
+      export default Projj
+      
+      
+      
+      
+      
+      
+    
+
